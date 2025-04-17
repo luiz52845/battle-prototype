54654df16b3b3db835339ca6290ab145d932767d
Luiz Fernandes <luizes_@hotmail.com>
2025-04-16 22:32:43 -0300

    Update WebSocketConfig.java

--- src/main/java/com/batle/batle/config/WebSocketConfig.java
+++ src/main/java/com/batle/batle/config/WebSocketConfig.java
@@ -2,7 +2,7 @@ package com.batle.batle.config;
 
 import com.batle.batle.handler.GameSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
